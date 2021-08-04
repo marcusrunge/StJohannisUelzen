4dae451e7b574d88a0d8dc4d1f0d8f56a8879f24
Marcus Runge <marcus.runge@outlook.com>
2021-08-04 21:04:17 +0200

    ADDED hilt

---
+++ app/src/main/java/com/marcusrunge/stjohannisuelzen/StJohannisUelzenApplication.kt
@@ -0,0 +1,8 @@
+package com.marcusrunge.stjohannisuelzen
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StJohannisUelzenApplication : Application(){
+}
