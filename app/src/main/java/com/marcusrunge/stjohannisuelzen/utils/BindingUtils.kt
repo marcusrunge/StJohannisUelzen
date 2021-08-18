95187514dd572b40f1b3cb58d07db6b95b955361
Marcus Runge <marcus.runge@outlook.com>
2021-08-18 21:28:16 +0200

    ADDED converter

--- app/src/main/java/com/marcusrunge/stjohannisuelzen/utils/BindingUtils.kt
+++ app/src/main/java/com/marcusrunge/stjohannisuelzen/utils/BindingUtils.kt
@@ -2,7 +2,9 @@ package com.marcusrunge.stjohannisuelzen.utils
 
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.EditText
 import androidx.databinding.BindingAdapter
+import androidx.databinding.InverseBindingAdapter
 import androidx.lifecycle.LiveData
 
 object BindingUtils {
@@ -13,5 +15,16 @@ object BindingUtils {
             view.webViewClient = WebViewClient()
             view.loadUrl(it)
         }
+    }
+
+    @InverseBindingAdapter(attribute = "goBack")
+    @JvmStatic fun getGoBack(view: WebView): ()->Unit {
+        view
+        return { view.goBack() }
+    }
+
+    @InverseBindingAdapter(attribute = "canGoBack", event = "canGoBackAttrChanged")
+    @JvmStatic fun getCanGoBack(view: WebView): Boolean {
+        return view.canGoBack()
     }
 }
