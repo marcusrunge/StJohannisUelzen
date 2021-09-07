b0a28eaa37e17c5f873c70a0aacc7ab230eda24c
Marcus Runge <marcus.runge@outlook.com>
2021-09-07 19:17:42 +0200

    added theme preferences

--- app/src/main/java/com/marcusrunge/stjohannisuelzen/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/marcusrunge/stjohannisuelzen/ui/settings/SettingsFragment.kt
@@ -3,8 +3,15 @@ package com.marcusrunge.stjohannisuelzen.ui.settings
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
 import com.marcusrunge.stjohannisuelzen.R
+import com.marcusrunge.stjohannisuelzen.core.interfaces.Core
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SettingsFragment : PreferenceFragmentCompat() {
+
+    @Inject
+    lateinit var core: Core
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
