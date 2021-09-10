be244c35cedd3c4a07d102039d34ff7f93fac276
Marcus Runge <marcus.runge@outlook.com>
2021-09-10 21:24:50 +0200

    fixed dark theme change crash

--- app/src/main/java/com/marcusrunge/stjohannisuelzen/MainActivity.kt
+++ app/src/main/java/com/marcusrunge/stjohannisuelzen/MainActivity.kt
@@ -6,6 +6,8 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -13,7 +15,6 @@ import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.marcusrunge.stjohannisuelzen.core.interfaces.Core
 import com.marcusrunge.stjohannisuelzen.databinding.MainActivityBinding
-import com.marcusrunge.stjohannisuelzen.interfaces.Navigation
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -21,28 +22,27 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: MainActivityBinding
+    private lateinit var navController: NavController
 
     @Inject
     lateinit var core: Core
-
-    @Inject
-    lateinit var navigation: Navigation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        navigation.activate()
 
         val navView: BottomNavigationView = binding.navView
+
+        navController = findNavController(R.id.nav_host_fragment_main_activity)
 
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_web, R.id.navigation_media, R.id.navigation_counseling
             )
         )
-        setupActionBarWithNavController(navigation.navController, appBarConfiguration)
-        navView.setupWithNavController(navigation.navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
         OssLicensesMenuActivity.setActivityTitle(getString(R.string.custom_license_title))
     }
 
@@ -55,11 +55,11 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.navigation_settings -> {
-                navigation.navController.navigate(R.id.navigation_settings)
+                navController.navigate(R.id.navigation_settings)
                 true
             }
             R.id.navigation_privacy -> {
-                navigation.navController.navigate(R.id.navigation_privacy)
+                navController.navigate(R.id.navigation_privacy)
                 true
             }
             R.id.navigation_licenses -> {
@@ -67,11 +67,11 @@ class MainActivity : AppCompatActivity() {
                 true
             }
             R.id.navigation_terms -> {
-                navigation.navController.navigate(R.id.navigation_terms)
+                navController.navigate(R.id.navigation_terms)
                 true
             }
             android.R.id.home -> {
-                navigation.navigateBack()
+                navController.popBackStack()
                 true
             }
             else -> super.onOptionsItemSelected(item)
--- app/src/main/java/com/marcusrunge/stjohannisuelzen/interfaces/Navigation.kt
+++
@@ -1,9 +0,0 @@
-package com.marcusrunge.stjohannisuelzen.interfaces
-
-import androidx.navigation.NavController
-
-interface Navigation {
-    val navController: NavController
-    fun activate()
-    fun navigateBack()
-}
