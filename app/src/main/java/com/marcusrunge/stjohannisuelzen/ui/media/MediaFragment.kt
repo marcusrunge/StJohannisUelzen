294d14b0ba6495a97ce4036377249f7d0f5fa6e4
Marcus Runge <marcus.runge@outlook.com>
2021-09-13 19:52:22 +0200

    added init error toast

--- app/src/main/java/com/marcusrunge/stjohannisuelzen/ui/media/MediaFragment.kt
+++ app/src/main/java/com/marcusrunge/stjohannisuelzen/ui/media/MediaFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -54,7 +55,7 @@ class MediaFragment : Fragment() {
                     p0: YouTubePlayer.Provider?,
                     p1: YouTubeInitializationResult?
                 ) {
-                    //TODO("Not yet implemented")
+                    Toast.makeText(context, p1?.name, Toast.LENGTH_LONG).show()
                 }
             })
     }
