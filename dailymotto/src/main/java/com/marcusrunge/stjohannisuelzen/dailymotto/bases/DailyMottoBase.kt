a882bdc8faee6419418bf57821209a577fa3aeb9
Marcus Runge <marcus.runge@outlook.com>
2021-10-23 19:36:13 +0200

    added loadDailyMottos

--- dailymotto/src/main/java/com/marcusrunge/stjohannisuelzen/dailymotto/bases/DailyMottoBase.kt
+++ dailymotto/src/main/java/com/marcusrunge/stjohannisuelzen/dailymotto/bases/DailyMottoBase.kt
@@ -1,11 +1,23 @@
 package com.marcusrunge.stjohannisuelzen.dailymotto.bases
 
 import android.content.Context
+import com.marcusrunge.stjohannisuelzen.dailymotto.R
+
 import com.marcusrunge.stjohannisuelzen.dailymotto.interfaces.Advice
 import com.marcusrunge.stjohannisuelzen.dailymotto.interfaces.DailyMotto
 import com.marcusrunge.stjohannisuelzen.dailymotto.interfaces.Motto
 
+
 internal abstract class DailyMottoBase(internal val context: Context?) : DailyMotto {
     protected lateinit var _advice: Advice
     protected lateinit var _motto: Motto
+
+    init {
+        //Test only
+        loadDailyMottos()
+    }
+
+    private fun loadDailyMottos(){
+        val parser = context?.resources?.getXml(R.xml.dailymottos)
+    }
 }
