a9058672d2b075c1082f7e982ed38cbf74bde75c
Marcus Runge <marcus.runge@outlook.com>
2021-10-24 20:41:33 +0200

    refactored loadDailyMottos

--- dailymotto/src/main/java/com/marcusrunge/stjohannisuelzen/dailymotto/bases/DailyMottoBase.kt
+++ dailymotto/src/main/java/com/marcusrunge/stjohannisuelzen/dailymotto/bases/DailyMottoBase.kt
@@ -6,6 +6,8 @@ import com.marcusrunge.stjohannisuelzen.dailymotto.R
 import com.marcusrunge.stjohannisuelzen.dailymotto.interfaces.Advice
 import com.marcusrunge.stjohannisuelzen.dailymotto.interfaces.DailyMotto
 import com.marcusrunge.stjohannisuelzen.dailymotto.interfaces.Motto
+import com.marcusrunge.stjohannisuelzen.dailymotto.models.Losungen
+import org.xmlpull.v1.XmlPullParser
 
 
 internal abstract class DailyMottoBase(internal val context: Context?) : DailyMotto {
@@ -13,11 +15,40 @@ internal abstract class DailyMottoBase(internal val context: Context?) : DailyMo
     protected lateinit var _motto: Motto
 
     init {
-        //Test only
         loadDailyMottos()
     }
 
+    internal val DailyMottos = mutableListOf<Losungen?>()
+
     private fun loadDailyMottos(){
-        val parser = context?.resources?.getXml(R.xml.dailymottos)
+        val parser = context?.resources?.getXml(R.xml.losungen)
+        if (parser != null) {
+            var eventType = parser.eventType
+            var losungen:Losungen?=null
+            var text:String?=null
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                val tagName = parser.name
+                when (eventType){
+                    XmlPullParser.START_TAG->if (tagName.equals("Losungen")) {
+                        losungen =Losungen()
+                    }
+                    XmlPullParser.TEXT ->text = parser.text
+                    XmlPullParser.END_TAG->{
+                        when(tagName){
+                            "Losungen"->DailyMottos.add(losungen)
+                            "Datum"->losungen?.Datum=text
+                            "Wtag"->losungen?.Wtag=text
+                            "Sonntag"->losungen?.Sonntag=text
+                            "Losungstext"->losungen?.Losungstext=text
+                            "Losungsvers"->losungen?.Losungsvers=text
+                            "Lehrtext"->losungen?.Lehrtext=text
+                            "Lehrtextvers"->losungen?.Lehrtextvers=text
+                        }
+                    }
+                }
+                eventType = parser.next();
+            }
+        }
+        parser?.close()
     }
 }
