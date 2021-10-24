0e92aa64f6e36ff3bebc00c79094793c890432d9
Marcus Runge <marcus.runge@outlook.com>
2021-10-24 12:33:29 +0200

    REFACTORED losungen

---
+++ dailymotto/src/main/java/com/marcusrunge/stjohannisuelzen/dailymotto/models/Losung.kt
@@ -0,0 +1,11 @@
+package com.marcusrunge.stjohannisuelzen.dailymotto.models
+
+data class Losung(
+    val Datum: String?,
+    val Wtag: String?,
+    val Sonntag: String?,
+    val Losungstext: String?,
+    val Losungsvers: String?,
+    val Lehrtext: String?,
+    val Lehrtextvers: String?
+)
