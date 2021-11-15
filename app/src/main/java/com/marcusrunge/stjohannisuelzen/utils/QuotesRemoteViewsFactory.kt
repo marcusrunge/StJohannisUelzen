4f23714d5bc95e2997e87ffee1892255ed22c75f
Marcus Runge <marcus.runge@outlook.com>
2021-11-15 18:40:47 +0100

    refactored getViewAt

--- app/src/main/java/com/marcusrunge/stjohannisuelzen/utils/QuotesRemoteViewsFactory.kt
+++ app/src/main/java/com/marcusrunge/stjohannisuelzen/utils/QuotesRemoteViewsFactory.kt
@@ -39,9 +39,7 @@ class QuotesRemoteViewsFactory(
         remoteViews.apply {
             setTextViewText(R.id.quote_content, quote.content)
             setTextViewText(R.id.quote_verse, quote.verse)
-            if (quotes.size > 1 && position % 2 == 0) {
-                setViewVisibility(R.id.quote_divider, View.VISIBLE)
-            } else {
+            if (quotes.size == 1 || (quotes.size>1 && position ==0)) {
                 setViewVisibility(R.id.quote_divider, View.GONE)
             }
         }
