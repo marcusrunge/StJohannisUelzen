984df815c8ab2add544d2cbdba545baf0a817d88
Marcus Runge <marcus.runge@outlook.com>
2021-12-27 14:56:34 +0100

    added documentation to apiconnect

--- apiconnect/src/main/java/com/marcusrunge/stjohannisuelzen/apiconnect/interfaces/YouTube.kt
+++ apiconnect/src/main/java/com/marcusrunge/stjohannisuelzen/apiconnect/interfaces/YouTube.kt
@@ -3,6 +3,13 @@ package com.marcusrunge.stjohannisuelzen.apiconnect.interfaces
 import com.marcusrunge.stjohannisuelzen.apiconnect.models.YoutubeSearchList
 
 interface YouTube {
+    /**
+     * Gets the YouTube search list.
+     * @param key the API key
+     * @param channelId the YouTube channel ID.
+     * @param onError the error callback.
+     * @param onSuccess the success callback containing the list.
+     */
     suspend fun getYoutubeSearchList(
         key: String?,
         channelId: String?,
